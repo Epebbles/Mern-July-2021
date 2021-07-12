8c14fe6ffadd183167dcc5eacfe25ee5413f0d63
Evan Pebbles <78051545+Epebbles@users.noreply.github.com>
2021-07-12 12:48:54 -0700

    Prop-it-up

---
+++ React/prop-it-up/src/components/PersonCard.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const PersonCard = (props) => {
+    const { firstName, lastName, hair } = props;
+    return (
+        <div className="container">
+            <h1>
+                {lastName}, {firstName}
+            </h1>
+            <p>Age: {props.age}</p>
+            <p>Hair Color: {hair}</p>
+        </div>
+    );
+}
+
+export default PersonCard;
