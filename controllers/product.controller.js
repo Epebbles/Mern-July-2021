abbfc28de0de4af670c7f49f87452b0ad2d00d9d
Evan Pebbles <78051545+Epebbles@users.noreply.github.com>
2021-08-07 21:43:36 -0700

    PM

---
+++ controllers/product.controller.js
@@ -0,0 +1,16 @@
+const Product = require('../models/product.model');
+
+module.exports.index = (req, res) => {
+    res.json({ message: "Suck this dickeh!"})
+}
+
+module.exports.createProduct = (req, res) => {
+    const { title, price, description } = req.body;
+    Product.create({
+        title,
+        price,
+        description
+    })
+        .then(product => res.json(product))
+        .catch(err => res.json(err));
+}
